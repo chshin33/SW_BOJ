bac31057b79475fc72e9c4500b4663b8dc0e6571
Jeong <Jeong@DESKTOP-KEER0C2>
2017-06-23 00:41:26 +0900

    ..

--- src/BinarySearch_2805.java
+++ src/BinarySearch_2805.java
@@ -1,62 +1,60 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BinarySearch_2805 {
-	static int N, M;
-	static int[] input;
+	static int N;
+	static int M;
+	static long[] input;
 	static int Answer;
+	static int DEBUG = 0;
 
 	public static void main(String[] args) throws Exception{
 		System.setIn(new FileInputStream("sample/sample_BinarySearch_2805.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		
-		input = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++){
-			input[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(input);
-		
-		st = new StringTokenizer(br.readLine());
 		M = Integer.parseInt(st.nextToken());
 		
-		int val;
-		st = new StringTokenizer(br.readLine());
-
-		Answer = checkHeight();
+		int max = 0;
+		input = new long[N];
+		int data=0;
+		st = new StringTokenizer(br.readLine());		
+		for(int i=0; i<N;i++){
+			data = Integer.parseInt(st.nextToken());
+			input[i] = data; 
+			max = Math.max(max, data);
+		}
 		
-		System.out.println(Answer);
-
-	}
-	
-	static int checkHeight(){
-		int start=0; 
-		int end=N-1;
+		if(DEBUG==1) System.out.println("max:" + max);
+		
+		int start=0;
+		int end =max;
 		int mid;
-		int sum=0;
+		long sum;
 		while(start<=end){
 			mid = (start+end)>>1;
-		
-			for(int i=mid; i<=N; i++){
-				sum += input[i] - input[mid]; 
+			sum=0;
+			for(int i=0; i<N; i++){
+				if(input[i]>mid){
+					sum += input[i] - mid;
+				}
 			}
+			if(DEBUG==1) System.out.printf("start:%d, end:%d, mid:%d, sum:%d, M:%d\n", start, end, mid, sum, M);
 			
-			if(sum == M){
-				return input[mid];
+			if(sum > M){
+				start = mid +1;
+				Answer = mid;
 			}else if(sum < M){
 				end = mid -1;
-			}else{
-				start = mid +1;
+			}else if(sum==M){
+				Answer = mid;
+				break;
 			}
 		}
-		return -(start+1);
+		
+		System.out.println(Answer);
+		
 	}
-
 }
---
+++ src/BinarySearch_2805_1.java
@@ -0,0 +1,114 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BinarySearch_2805_1 {
+	static int N;
+	static long M;
+	static long[] input;
+	static long[] psum;
+	static long Answer;
+	static int DEBUG=0;
+
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("sample/sample_BinarySearch_2805.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		input = new long[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++){
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(input);
+		
+		if(DEBUG==1) System.out.println(Arrays.toString(input));
+		
+		psum = new long[N];
+		psum[0] = input[0];
+		for(int i=1; i<N; i++){
+			psum[i] = psum[i-1] + input[i];
+		}
+		
+		if(DEBUG==1) System.out.println(Arrays.toString(psum));
+		
+		Answer = getMinHeight();
+		
+		System.out.println(Answer);
+
+	}
+	
+	static long getMinHeight(){
+		int start=0; 
+		int end=N-1;
+		int mid;
+		long val;
+		int pos = -1;
+		int left=0, right=0;
+		long retVal=0;
+		while(start<=end){
+			mid = (start+end)>>1;
+			if(DEBUG==1) System.out.println("mid: " + mid);
+			val = devideSum(mid, N-1);
+			if(DEBUG==1) System.out.printf("devideSum:(%d,%d) = %d\n", mid, N-1, val);
+			
+			if(val == M){
+				pos = mid;
+				break;
+			}else if(val <M){//왼쪽으로 이동
+				end = mid -1;
+				right = mid;
+			}else if(val > M){//오른쪽으로 이동
+				start = mid + 1;
+				left = mid;
+			}
+		}
+		
+		if(pos > -1){
+			retVal = input[pos];
+		}else{
+			//left와  right사이에 있는 높이가 답이다.
+			if(DEBUG==1) System.out.printf("left:%d, right:%d\n", left, right);
+			long leftH = input[left];
+			long rightH = input[right];
+			int sum;
+			//for(int h = leftH+1; h<=rightH-1; h++){
+			for(long h = leftH; h<=rightH; h++){
+				sum = 0;
+				for(int i=left; i<=N-1; i++){
+					sum +=  Math.max(input[i] - h, 0);
+				}
+				if(sum ==M){
+					retVal = h;
+					break;
+				}else if(sum < M){
+					retVal = h-1;
+					break;
+				}
+			}
+		}
+		return retVal;
+	}
+	
+	static long devideSum(int start, int end){
+		long val;
+		val = rsum(start, end);
+		val -= input[start] * (end-start+1);
+		return val;
+	}
+	
+	static long rsum(int start, int end){
+		if(start>=1){
+			return psum[end]-psum[start-1];	
+		}else{
+			return psum[end];
+		}
+	}
+
+}
